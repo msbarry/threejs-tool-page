04d706a3c1febff58c083fc3f1f41f1d57602dea
Mike Barry <msb5014@gmail.com>
2014-03-11 05:54:19 -0400

    Built threejs from commit fa30ef1 on branch master

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ $(function () {
   // Parallax scrolling
   var $a = $(".bs-header-after"),
       $w = $('body');
-  if (!('ontouchstart' in window)) {
+  if (!Modernizr.touch && Modernizr.csstransforms3d) {
     $(window).unbind("scroll").scroll(function () {
       $a.css('transform', 'translate3d(0, ' + ($w.scrollTop() * 0.15) + 'px, 0)');
       $('.bs-top').css('opacity', 1 - ($w.scrollTop() / ($a.height() - 500)));
@@ -15,14 +15,16 @@ $(function () {
   window.prettyPrint();
 
   if (Modernizr.webglenabled) {
+    var width = 200;
+    var height = 140;
     (function spinningSphere() {
       var THREE = window.THREE;
       var $container = $("#spinner");
       var scene = new THREE.Scene();
-      var camera = new THREE.PerspectiveCamera( 45, $container.width() / $container.height(), 0.1, 1000 );
+      var camera = new THREE.PerspectiveCamera( 45, width / height, 0.1, 1000 );
       camera.position.z = 10;
       var renderer = new THREE.WebGLRenderer();
-      renderer.setSize($container.width(), $container.height());
+      renderer.setSize(width, height);
       $(renderer.domElement).appendTo($container);
       renderer.setClearColorHex(0xffffff, 1.0);
       renderer.clear();
@@ -38,11 +40,11 @@ $(function () {
       renderer.render(scene, camera);
 
       function animate(t) {
-        camera = new THREE.PerspectiveCamera( 45, $container.width() / $container.height(), 0.1, 1000 );
-        renderer.setSize($container.width() -2, $container.height()-2);
-        camera.position.x = Math.sin(t/500)*10;
+        camera = new THREE.PerspectiveCamera( 45, width / height, 0.1, 1000 );
+        renderer.setSize(width -2, height-2);
+        camera.position.x = Math.sin(t/500)*8;
         camera.position.y = 0;
-        camera.position.z = Math.cos(t/500)*10;
+        camera.position.z = Math.cos(t/500)*8;
         camera.lookAt(scene.position);
         renderer.render(scene, camera);
         window.requestAnimationFrame(animate, renderer.domElement);
@@ -55,7 +57,7 @@ $(function () {
       var $container = $("#staticsphere");
       var scene = new THREE.Scene();
       var renderer = new THREE.WebGLRenderer();
-      renderer.setSize($container.width(), $container.height());
+      renderer.setSize(width, height);
       $(renderer.domElement).appendTo($container);
       renderer.setClearColorHex(0xffffff, 1.0);
       renderer.clear();
@@ -68,8 +70,8 @@ $(function () {
       var light = new THREE.SpotLight();
       light.position.set( -10, 20, 16 );
       scene.add(light);
-      var camera = new THREE.PerspectiveCamera( 45, $container.width() / $container.height(), 0.1, 1000 );
-      camera.position.z = 10;
+      var camera = new THREE.PerspectiveCamera( 45, width / height, 0.1, 1000 );
+      camera.position.z = 8;
       camera.position.x = 0;
       camera.position.y = 0;
       camera.lookAt(scene.position);
